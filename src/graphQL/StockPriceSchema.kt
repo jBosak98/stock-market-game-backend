464e76155dd05ca42288691e8fe3fe66dc4ca89c
jBosak <jbosak98@gmail.com>
2020-07-12 00:01:51 +0200

    dataloader resolver, added company to stockPrice query

--- src/graphQL/StockPriceSchema.kt
+++ src/graphQL/StockPriceSchema.kt
@@ -1,6 +1,7 @@
 package com.ktor.stock.market.game.jbosak.graphQL
 
 import com.ktor.stock.market.game.jbosak.service.getRealTimeSecurityPrice
+import graphql.schema.AsyncDataFetcher.async
 import graphql.schema.idl.TypeRuntimeWiring
 
 fun getStockPriceSchema() =
@@ -21,6 +22,7 @@ fun getStockPriceSchema() =
             dataSource: String!
             securityExternalId: String!
             securityTicker: String!
+            company: Company!
         }
 
     """
@@ -28,9 +30,32 @@ fun getStockPriceSchema() =
 fun TypeRuntimeWiring.Builder.stockPriceMutationResolvers() =
     this
 
-fun TypeRuntimeWiring.Builder.stockPriceQueryResolvers() =
-    this.dataFetcher("getPrices"){ env ->
-        val ticker = env.arguments["ticker"] as String
-        val response = getRealTimeSecurityPrice(ticker)
-        listOf(response)
-    }
+
+fun TypeRuntimeWiring.Builder.stockPriceQueryResolvers(): TypeRuntimeWiring.Builder =
+    this
+        .dataFetcher("getPrices", async { env ->
+            val ticker = env.arguments["ticker"] as String
+            val resolvedDataLoaders = dataloaderResolver(env)
+            val response = getRealTimeSecurityPrice(ticker)
+            listOf(response).map { stockPrice ->
+                object {
+                    val id = stockPrice!!.id
+                    val updatedOn = stockPrice!!.updatedOn
+                    val lastPrice = stockPrice!!.lastPrice
+                    val lastTime = stockPrice!!.lastTime
+                    val bidPrice = stockPrice!!.bidPrice
+                    val askPrice = stockPrice!!.askPrice
+                    val askSize = stockPrice!!.askSize
+                    val openPrice = stockPrice!!.openPrice
+                    val highPrice = stockPrice!!.highPrice
+                    val lowPrice = stockPrice!!.lowPrice
+                    val exchangeVolume = stockPrice!!.exchangeVolume
+                    val marketVolume = stockPrice!!.marketVolume
+                    val dataSource = stockPrice!!.dataSource
+                    val securityExternalId = stockPrice!!.securityExternalId
+                    val securityTicker = stockPrice!!.securityTicker
+                    val company = resolvedDataLoaders["company"]?.load(ticker)
+                }
+            }
+        })
+
