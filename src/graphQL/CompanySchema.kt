48714c0da7446bf7ce9cb83f38bd12b00db97e23
jBosak <jbosak98@gmail.com>
2020-07-11 23:57:35 +0200

    Company data loader

--- src/graphQL/CompanySchema.kt
+++ src/graphQL/CompanySchema.kt
@@ -1,11 +1,15 @@
 package com.ktor.stock.market.game.jbosak.graphQL
 
+import com.ktor.stock.market.game.jbosak.model.Company
 import com.ktor.stock.market.game.jbosak.model.ConnectionArguments
 import com.ktor.stock.market.game.jbosak.repository.CompanyRepository.companiesSize
-import com.ktor.stock.market.game.jbosak.repository.CompanyRepository.findCompanies
+import com.ktor.stock.market.game.jbosak.repository.CompanyRepository.findCompany
 import com.ktor.stock.market.game.jbosak.service.getCompanies
 import com.ktor.stock.market.game.jbosak.utils.convertToObject
 import graphql.schema.idl.TypeRuntimeWiring
+import org.dataloader.BatchLoader
+import org.dataloader.DataLoader
+import java.util.concurrent.CompletableFuture
 
 fun getCompanySchema() =
     """
@@ -35,3 +39,19 @@ fun TypeRuntimeWiring.Builder.companyQueryResolvers() =
         }
     }
 
+fun companyDataLoader(): DataLoader<Any, Company>? {
+    val loaderById = BatchLoader<Any, Company> { keys ->
+        CompletableFuture.supplyAsync {
+            keys.map {
+                when (it) {
+                    is String -> findCompany(ticker = it)
+                    is Int -> findCompany(id = it)
+                    else -> null
+                }
+            }
+        }
+    }
+    val companiesDataLoader = DataLoader.newDataLoader(loaderById)
+    return companiesDataLoader
+
+}
