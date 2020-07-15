95ca244569f04a791ff4af1dc493adcc71e42743
jBosak <jbosak98@gmail.com>
2020-07-15 14:54:10 +0200

    removed api key

--- src/server/initExternalApi.kt
+++ src/server/initExternalApi.kt
@@ -6,5 +6,5 @@ import com.intrinio.invoker.auth.ApiKeyAuth
 fun initExternalApi(){
     val client = Configuration.getDefaultApiClient()
     val auth = client.getAuthentication("ApiKeyAuth") as ApiKeyAuth
-    auth.apiKey = "OjdkYjE0NmNmM2YzZWM5ZDFlMDQ5YzlmMmFkZTU2MDFk"
+    auth.apiKey = ""
 }
