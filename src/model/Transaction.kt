4fd8c0c2a6a8bd00041d9ca5ef57a5914192cdd1
jBosak <jbosak98@gmail.com>
2020-10-07 12:10:44 +0200

    Transaction models

--- src/model/Transaction.kt
+++ src/model/Transaction.kt
@@ -1,5 +1,6 @@
 package com.ktor.stock.market.game.jbosak.model
 
+import com.ktor.stock.market.game.jbosak.model.graphql.TransactionGraphQL
 import org.joda.time.DateTime
 
 data class Transaction(
@@ -11,3 +12,14 @@ data class Transaction(
     val createdAt:DateTime,
     val type:TransactionType
 )
+
+fun Transaction.toGraphQL() =
+    TransactionGraphQL(
+        id = id,
+        playerId = playerId,
+        companyId = companyId,
+        pricePerShare = pricePerShare,
+        quantity = quantity,
+        createdAt = createdAt,
+        type = type.toString()
+    )
---
+++ src/model/graphql/TransactionGraphQL.kt
@@ -0,0 +1,14 @@
+package com.ktor.stock.market.game.jbosak.model.graphql
+
+import org.joda.time.DateTime
+
+data class TransactionGraphQL(
+    val id:Int,
+    val playerId:Int,
+    val companyId:Int,
+    val pricePerShare:Float,
+    val quantity:Int,
+    val createdAt: DateTime,
+    val type: String
+)
+
