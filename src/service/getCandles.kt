e824091190356e23d6f6480df8dea9e946155843
jBosak <jbosak98@gmail.com>
2020-10-17 23:16:20 +0200

    CandleCache implemented

--- src/service/getCandles.kt
+++ src/service/getCandles.kt
@@ -1,5 +1,7 @@
 package com.ktor.stock.market.game.jbosak.service
 
+import arrow.core.extensions.list.foldable.exists
+import arrow.syntax.function.pipe
 import com.finnhub.api.apis.DefaultApi
 import com.ktor.stock.market.game.jbosak.model.CandlesResolution
 import com.ktor.stock.market.game.jbosak.model.SingleCandle
@@ -7,32 +9,20 @@ import com.ktor.stock.market.game.jbosak.model.toCandle
 import com.ktor.stock.market.game.jbosak.repository.CandleRepository
 import com.ktor.stock.market.game.jbosak.utils.toUnixTime
 import org.joda.time.DateTime
-import org.joda.time.Days
-import org.joda.time.Minutes
-import org.joda.time.Months
+import org.joda.time.Interval
+import org.joda.time.LocalDate
 
 
 fun getCandles(ticker:String, res:CandlesResolution, from:DateTime, to:DateTime?): List<SingleCandle> {
     val toDate = to?: DateTime.now()
-    val candles = CandleRepository.find(ticker, res, from, toDate)
-    val isValid = when(res){
-        CandlesResolution.ONE_MINUTE ->
-            Minutes.minutesBetween(from, to).minutes == candles.size
-        CandlesResolution.FIVE_MINUTES ->
-            Minutes.minutesBetween(from, to).minutes == candles.size * 5
-        CandlesResolution.FIFTEEN_MINUTES ->
-            Minutes.minutesBetween(from, to).minutes == candles.size * 15
-        CandlesResolution.THIRTY_MINUTES ->
-            Minutes.minutesBetween(from, to).minutes == candles.size * 30
-        CandlesResolution.ONE_DAY ->
-            Days.daysBetween(from, to).days == candles.size
-        CandlesResolution.ONE_WEEK ->
-            Days.daysBetween(from, to).days == candles.size * 7
-        CandlesResolution.ONE_MONTH ->
-            Months.monthsBetween(from, to).months == candles.size
-    }
+    val isValid = CandleCache[ticker]
+        ?.candleIntervals
+        ?.find { it.resolution == res }
+        ?.intervals
+        ?.exists { from.isAfter(it.start) && toDate.isBefore(it.end) }
+        ?: false
 
-    return if(isValid) candles
+    return if(isValid) CandleRepository.find(ticker, res, from, toDate)
     else refetch(ticker, res, from, toDate)
 }
 
@@ -43,15 +33,31 @@ private fun refetch(
     from:DateTime,
     to:DateTime
 ): List<SingleCandle> {
+    val normalizedFrom = when(res){
+        CandlesResolution.ONE_MONTH -> DateTime((from.year().get() - 1).toString())
+        CandlesResolution.ONE_WEEK -> DateTime((from.year().get()).toString())
+        CandlesResolution.ONE_DAY -> DateTime((from.year().get()).toString())
+        else -> LocalDate(from).toDateTimeAtStartOfDay()
+    }
+        .pipe { if(it.isAfterNow) DateTime.now() else it }
+    val normalizedTo = when(res){
+        CandlesResolution.ONE_MONTH -> DateTime((to.year().get() + 2).toString())
+        CandlesResolution.ONE_WEEK -> DateTime((to.year().get() + 1).toString())
+        CandlesResolution.ONE_DAY -> DateTime((to.year().get() + 1).toString())
+        else -> LocalDate(to.plusDays(1)).toDateTimeAtStartOfDay()
+    }
+        .pipe { if(it.isAfterNow) DateTime.now() else it }
+
     val candles = DefaultApi()
         .stockCandles(
             symbol = ticker,
             resolution = res.resolution,
-            from = from.toUnixTime(),
-            to = to.toUnixTime(),
+            from = normalizedFrom.toUnixTime(),
+            to = normalizedTo.toUnixTime(),
             adjusted = true.toString()
         ).toCandle()
-
+    val interval = Interval(normalizedFrom, normalizedTo)
+    CandleCache.update(ticker,interval,res)
     CandleRepository.upsert(candles,ticker,res)
     return  CandleRepository.find(ticker, res,from)
 }
---
+++ test/utils/CandleCacheTest.kt
@@ -0,0 +1,59 @@
+package utils
+
+import com.ktor.stock.market.game.jbosak.model.CandlesResolution
+import com.ktor.stock.market.game.jbosak.service.CandleCache
+import org.joda.time.DateTime
+import org.joda.time.Interval
+import org.junit.jupiter.api.Test
+
+internal class CandleCacheTest {
+
+    @Test
+    fun update() {
+        CandleCache.update("AAPL",
+            Interval(
+                DateTime(2020,1,1,1,1)
+                ,DateTime(2020,1,3,1,1)),
+            CandlesResolution.THIRTY_MINUTES
+        )
+        assert(CandleCache["AAPL"]!!.candleIntervals.size == 1)
+        assert(CandleCache["AAPL"]!!.candleIntervals[0].intervals.size == 1)
+        assert(CandleCache["AAPL"]!!.candleIntervals[0].intervals[0].start.toDateTime() == DateTime(2020,1,1,1,1))
+        assert(CandleCache["AAPL"]!!.candleIntervals[0].intervals[0].end.toDateTime() == DateTime(2020,1,3,1,1))
+
+        CandleCache.update("AAPL",
+            Interval(
+                DateTime(2020,1,2,1,1),
+                DateTime(2020,1,5,1,1)),
+            CandlesResolution.THIRTY_MINUTES
+        )
+        assert(CandleCache["AAPL"]!!.candleIntervals.size == 1)
+        assert(CandleCache["AAPL"]!!.candleIntervals[0].intervals.size == 1)
+        assert(CandleCache["AAPL"]!!.candleIntervals[0].intervals[0].start.toDateTime() == DateTime(2020,1,1,1,1))
+        assert(CandleCache["AAPL"]!!.candleIntervals[0].intervals[0].end.toDateTime() == DateTime(2020,1,5,1,1))
+
+
+        CandleCache.update("AAPL",
+            Interval(
+                DateTime(2020,1,7,1,1),
+                DateTime(2020,1,8,1,1)),
+            CandlesResolution.THIRTY_MINUTES
+        )
+        assert(CandleCache["AAPL"]!!.candleIntervals.size == 1)
+        assert(CandleCache["AAPL"]!!.candleIntervals[0].intervals.size == 2)
+        assert(CandleCache["AAPL"]!!.candleIntervals[0].intervals[0].start.toDateTime() == DateTime(2020,1,1,1,1))
+        assert(CandleCache["AAPL"]!!.candleIntervals[0].intervals[0].end.toDateTime() == DateTime(2020,1,5,1,1))
+
+        CandleCache.update("AAPL",
+            Interval(
+                DateTime(2020,1,4,1,1),
+                DateTime(2020,1,7,1,1)),
+            CandlesResolution.THIRTY_MINUTES
+        )
+
+        assert(CandleCache["AAPL"]!!.candleIntervals.size == 1)
+        assert(CandleCache["AAPL"]!!.candleIntervals[0].intervals.size == 1)
+        assert(CandleCache["AAPL"]!!.candleIntervals[0].intervals[0].start.toDateTime() == DateTime(2020,1,1,1,1))
+        assert(CandleCache["AAPL"]!!.candleIntervals[0].intervals[0].end.toDateTime() == DateTime(2020,1,8,1,1))
+    }
+}
